82f7ff0e49aa5361453079af3fcb68569e94d7e7
Thiago <thiagodesouza.dev@gmail.com>
2021-11-02 18:39:34 -0300

    corrigindo alias

--- desafio15.sql
+++ desafio15.sql
@@ -2,7 +2,7 @@ USE hr;
 DELIMITER $$
 CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(50))
 BEGIN
-SELECT ROUND(AVG(SALARY), 2) AS `media salarial`
+SELECT ROUND(AVG(SALARY), 2) AS `Média salarial`
 from employees AS e
 INNER JOIN jobs AS j
 ON e.JOB_ID = j.JOB_ID
